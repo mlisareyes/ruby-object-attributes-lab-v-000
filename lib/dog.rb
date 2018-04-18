8a8aa8c55fa71c5dc69e66081dc02549477be1c8
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-04-18 05:21:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog
-  def initialize(name)
+  def name
+    @name
+  end
+
+  def name=(name)
     @name = name
   end
 
-  def name
-    @name
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
   end
 end
